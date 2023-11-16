9590fe145ed3ff9fbea202856ac16a9f661bde41
JEONSEUNGREE <jws08219@naver.com>
2023-11-16 09:19:18 +0900

    feat: 회원 서비스 java docs 설정 - UserServiceImpl javadocs 설정

--- tp-back-app/domain/tp-rdb-domain/src/main/java/com/trip/penguin/user/service/UserServiceImpl.java
+++ tp-back-app/domain/tp-rdb-domain/src/main/java/com/trip/penguin/user/service/UserServiceImpl.java
@@ -13,21 +13,40 @@ public class UserServiceImpl implements UserService {
 
     private final UserMSRepository userMSRepository;
 
+    /**
+     * 회원 가입
+     * @param userMS - 회티원 엔티티
+     * @return UserMS
+     */
     @Override
     public UserMS signUpUser(UserMS userMS) {
         return userMSRepository.save(userMS);
     }
 
+    /**
+     * 회원 정보 조회
+     * @param userMS - 회티원 엔티티
+     * @return Optional<UserMS>
+     */
     @Override
     public Optional<UserMS> getUser(UserMS userMS) {
         return userMSRepository.findById(userMS.getId());
     }
 
+    /**
+     * 회원 정보 수정
+     * @param userMS - 회원 엔티티
+     * @return UserMS
+     */
     @Override
     public UserMS updateUser(UserMS userMS) {
         return userMSRepository.save(userMS);
     }
 
+    /**
+     * 회원 탈퇴
+     * @param userMS - 회원 엔티티
+     */
     @Override
     public void deleteUser(UserMS userMS) {
         userMSRepository.deleteById(userMS.getId());
